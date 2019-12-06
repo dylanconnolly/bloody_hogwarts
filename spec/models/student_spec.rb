2c76ff5b65011022c333d1a190c22caa24df0477
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-06 10:15:23 -0700

    Add test to student model spec to test average_age method

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
+  describe "#average_age" do
+    it "returns the average age of all students" do
+      bob = Student.create(name: "Bob", age: 12, house: "Ravenclaw")
+      tom = Student.create(name: "Tom", age: 14, house: "Hufflepuff")
+      sam = Student.create(name: "Sam", age: 17, house: "Slytherin")
+
+      expect(Student.average_age).to eq (14.33)
+    end
+  end
+
 end
