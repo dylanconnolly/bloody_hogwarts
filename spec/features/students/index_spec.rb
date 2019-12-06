122f48d8bc77cf243d1db0c35230da5696491d74
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-06 09:31:40 -0700

    Create student index test

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "student index page" do
+  it "displays all students with their name, age, and house" do
+    bob = Student.create!(name: "Bob", age: 12, house: "Ravenclaw")
+    tom = Student.create!(name: "Tom", age: 14, house: "Hufflepuff")
+
+    visit '/students'
+
+    expect(page).to have_content("Bob")
+    expect(page).to have_content("12")
+    expect(page).to have_content("Ravenclaw")
+    expect(page).to have_content("Tom")
+    expect(page).to have_content("14")
+    expect(page).to have_content("Hufflepuff")
+  end
+end
