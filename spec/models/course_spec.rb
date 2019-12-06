c069b8aca42fe495800a8ec49370aee8c6a9345b
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-06 10:08:24 -0700

    Add test to course model spec to test student_count method

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe Course, type: :model do
     it {should have_many(:students).through(:student_courses)}
   end
 
+  describe "#student_count" do
+    it "returns number of students in course" do
+    potions = Course.create(name: "Potions")
+
+    bob = Student.create(name: "Bob", age: 12, house: "Ravenclaw")
+    tom = Student.create(name: "Tom", age: 14, house: "Hufflepuff")
+    sam = Student.create(name: "Sam", age: 17, house: "Slytherin")
+
+    expect(potions.student_count).to eq(0)
+
+    potions.students << bob
+
+    expect(potions.student_count).to eq(1)
+
+    potions.students << tom
+    potions.students << sam
+
+    expect(potions.student_count).to eq(3)
+    end
+  end
 end
